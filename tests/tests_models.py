336ac0679762f3eb598393f956e12fbae450b0cf
Osei-Nathan <nohenebeng001@st.ug.edu.gh>
2024-03-26 08:53:00 +0000

    Added new features

--- tests/tests_models.py
+++ tests/tests_models.py
@@ -6,19 +6,19 @@ class TestModel(APITestCase):
 
     def test_creates_user(self):
         user = User.objects.create_user(
-            'cryce', 'crycetruly@gmail.com', 'password123!@')
+            'nathanhenebeng', 'nathanoseihenebeng@gmail.com', 'password123!@')
         self.assertIsInstance(user, User)
         self.assertFalse(user.is_staff)
-        self.assertEqual(user.email, 'crycetruly@gmail.com')
+        self.assertEqual(user.email, 'nathanoseihenebeng@gmail.com')
 
     def test_raises_error_when_no_username_is_supplied(self):
         self.assertRaises(ValueError, User.objects.create_user, username="",
-                          email='crycetruly@gmail.com', password='password123!@')
+                          email='nathanoseihenebeng@gmail.com', password='password123!@')
 
     def test_raises_error_with_message_when_no_username_is_supplied(self):
         with self.assertRaisesMessage(ValueError, 'The given username must be set'):
             User.objects.create_user(
-                username='', email='crycetruly@gmail.com', password='password123!@')
+                username='', email='nathanoseihenebeng@gmail.com', password='password123!@')
 
     def test_raises_error_when_no_email_is_supplied(self):
         self.assertRaises(ValueError, User.objects.create_user,
@@ -32,16 +32,16 @@ class TestModel(APITestCase):
     def test_cant_create_super_user_with_no_is_staff_status(self):
         with self.assertRaisesMessage(ValueError, 'Superuser must have is_staff=True.'):
             User.objects.create_superuser(
-                username='username', email='crycetruly@gmail.com', password='password123!@', is_staff=False)
+                username='username', email='nathanoseihenebeng@gmail.com', password='password123!@', is_staff=False)
 
     def test_cant_create_super_user_with_no_super_user_status(self):
         with self.assertRaisesMessage(ValueError, 'Superuser must have is_superuser=True.'):
             User.objects.create_superuser(
-                username='username', email='crycetruly@gmail.com', password='password123!@', is_superuser=False)
+                username='username', email='nathanoseihenebeng@gmail.com', password='password123!@', is_superuser=False)
 
     def test_creates_super_user(self):
         user = User.objects.create_superuser(
-            'cryce', 'crycetruly@gmail.com', 'password123!@')
+            'nathanhenebeng', 'nathanoseihenebeng@gmail.com', 'password123!@')
         self.assertIsInstance(user, User)
         self.assertTrue(user.is_staff)
-        self.assertEqual(user.email, 'crycetruly@gmail.com')
+        self.assertEqual(user.email, 'nathanoseihenebeng@gmail.com')
--- todolistapi/urls.py
+++ todolistapi/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/auth/", include("authentication.urls")),
+    path("api/todos/", include("todos.urls")),
     
 ]
