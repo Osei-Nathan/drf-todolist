0cad264753245049cad7080e87dbd9127c43570f
Osei-Nathan <nohenebeng001@st.ug.edu.gh>
2024-04-02 12:26:02 +0000

    modified serializers

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -19,6 +19,7 @@ class LoginSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(
         max_length=128, min_length=6, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)  # Add this line
 
     class Meta:
         model = User
