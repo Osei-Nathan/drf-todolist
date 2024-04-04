67c7f02e3c3bb7c373edf071fec057c66aaed003
Osei-Nathan <nohenebeng001@st.ug.edu.gh>
2024-04-04 11:16:17 +0000

    Added refresh and access token

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 from rest_framework import status
 from authentication.serializers import RegisterSerializer, LoginSerializer
 from django.contrib.auth import authenticate
 from rest_framework.authentication import SessionAuthentication
-
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class AuthUserAPIView(APIView):
     authentication_classes = [SessionAuthentication]
@@ -28,15 +29,29 @@ class RegisterAPIView(APIView):
 
 class LoginAPIView(APIView):
     authentication_classes = []
+    serializer_class = LoginSerializer
 
     def post(self, request):
-        serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid():
-            email = serializer.validated_data.get('email')
-            password = serializer.validated_data.get('password')
-            user = authenticate(username=email, password=password)
-            if user:
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            else:
-                return Response({'message': "Invalid credentials, try again"}, status=status.HTTP_401_UNAUTHORIZED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        email = request.data.get('email', None)
+        password = request.data.get('password', None)
+
+        user = authenticate(username=email, password=password)
+
+        if user:
+            # Generate refresh and access tokens
+            refresh = RefreshToken.for_user(user)
+            token = {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            }
+            # Return JSON response with token and user data
+            return JsonResponse(
+                {"status": "success", "data": {"token": token, "user": user.username}},
+                status=status.HTTP_200_OK,
+                safe=False,
+            )
+        else:
+            return JsonResponse(
+                {'status': 'error', 'message': "Invalid credentials, try again"},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
